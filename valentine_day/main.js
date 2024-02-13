5059cafe644ac2b5aebabb9d60f88b2187b3c580
Fábián Levente <97455713+fablevi@users.noreply.github.com>
2024-02-13 21:05:56 +0100

    Update

--- valentine_day/main.js
+++ valentine_day/main.js
@@ -1,15 +1,17 @@
 import './style.css'
 
 const buttonOk = `<button class="btn btn-danger btn-lg button-ok">Leszek</button>`
-const buttonNever  = `<button class="btn btn-secondary btn-lg button-never">Nem leszek</button>`
+const buttonNever = `<button class="btn btn-secondary btn-lg button-never">Nem leszek</button>`
 const label = '<h1 class="label-text">Leszel a valentinom?</h1>'
 
-//document.getElementById('app').innerHTML = label + buttonOk + buttonNever;
+const newSpline = `<spline-viewer id="splineview" url="https://prod.spline.design/ZIpvESwQtwnlLLBi/scene.splinecode"></spline-viewer>`
 
-//buttonNever.style.position = "fixed"
-//buttonNever.style.right = "40%"
+const newGif = ` <div class="row gifLabel">
+                  <div class="col" id="gif"></div>
+                </div>`
 
 var neverbtn = document.getElementById('neverbtn')
+var okbtn = document.getElementById('okbtn')
 
 document.getElementById('label').innerHTML = label;
 document.getElementById('okbtn').innerHTML = buttonOk;
@@ -18,29 +20,38 @@ neverbtn.innerHTML = buttonNever;
 neverbtn.firstChild.style.position = "fixed"
 neverbtn.firstChild.style.right = "35%"
 
-var width, height;
-
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
+}
+
+function deleteSplineIcon() {
+  for (let i = 0; i < 25; i++) {
+    setTimeout(() => {
+      document.getElementById('splineview').shadowRoot.lastChild.children[3].remove()
+    }, 250)
+  }
 }
 
 //shadowRoot.lastChild.children[3].remove()
 document.getElementById('splineview')
 
-for (let i = 0; i < 5; i++) {
-  try{
-    setTimeout(() => {
-      document.getElementById('splineview').shadowRoot.lastChild.children[3].remove()
-    },500)
-  }catch(e){
-
-  }
-}
+deleteSplineIcon()
 
 neverbtn.firstChild.addEventListener("click", () => {
-  neverbtn.firstChild.style.right = getRandomInt(90) +"%"
-  neverbtn.firstChild.style.top = getRandomInt(90) +"%"
+  neverbtn.firstChild.style.right = getRandomInt(90) + "%"
+  neverbtn.firstChild.style.top = getRandomInt(90) + "%"
 })
+
+okbtn.firstChild.addEventListener("click", () => {
+  document.getElementById('splineview').remove()
+  document.body.innerHTML = newSpline + document.body.innerHTML
+  deleteSplineIcon()
+  document.getElementById('app').innerHTML = newGif
+
+})
+
+
+
 
 
 
